d0b363ed1acacaf665aab57d5371de991e323e05
Yuriy Zhyvko <yuriy.zhyvko@gmail.com>
2024-01-18 23:02:04 -0500

    1. created new api test -test_repo_can_be_found 2. created new api test - test_repo_cannot_be_found 3. created new api test - test_repo_with_single_char_can_be_found

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -8,3 +8,9 @@ class Github:
 
         return body
     
+    def search_repo(self, name):
+        r = requests.get("https://api.github.com/search/repositories",
+                        params={"q": name})
+        body = r.json()
+
+        return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -13,4 +13,20 @@ def test_user_not_exists(github_api):  # verifies that user doesn't exist
     #  print(r) - we need to run it only first time to find out what message we get when user is not registred, and then use it later for assert
     assert r["message"] == "Not Found"
 
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+     r = github_api.search_repo("become-qa-auto")
+     assert r["total_count"] == 54  # counts how many "become-qa-auto" repos are there
+     assert "become-qa-auto" in r["items"] [0] ["name"]  # verifies that first repo contains "become-qa-auto"
+    
 
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):  # verifies that "sergiibutenko_repo_non_exist" repo is not present
+    r = github_api.search_repo("sergiibutenko_repo_non_exist")
+    assert r["total_count"] == 0
+
+
+@pytest.mark.api  
+def test_repo_with_single_char_can_be_found(github_api): #  verifies that there is a repo with name that contains only 1 symbol
+    r = github_api.search_repo("s")
+    assert r["total_count"] != 0
