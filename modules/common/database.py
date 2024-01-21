580f8525e7366b1ea6ff564177ce1c148e39677f
Yuriy Zhyvko <yuriy.zhyvko@gmail.com>
2024-01-21 13:16:57 -0500

    1. created method for Database class - insert_product 2. created method for Database class - delete_product_by_id 3. created method for Database class - get_detailed_orders
    4. created Database test - test_product_insert
    5. created Database test - test_product_delete
    6. created Database test - test_detailed_order
    
    7. created .gitignore file

--- modules/common/database.py
+++ modules/common/database.py
@@ -41,7 +41,26 @@ class Database():
         return record
     
 
+    def insert_product(self, product_id, name, description, qnt):
+        query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
+        VALUES ({product_id}, '{name}', '{description}', {qnt})"
+        self.cursor.execute(query)
+        self.connection.commit() # confirmation of changes in db to prevent any updates by mistake
+
     
+    def delete_product_by_id(self, product_id):
+        query = f"DELETE FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
 
 
+    def get_detailed_orders(self)    :
+        query = "SELECT orders.id, customers.name, products.name,  \
+            products.description, orders.order_date \
+            FROM orders \
+            JOIN customers ON orders.customer_id = customers.id \
+            JOIN products ON orders.product_id = products.id"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall() # save result of the query in record variable
+        return record
     
--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -20,6 +20,7 @@ def test_check_all_users():
 def test_check_user_sergii():
     db = Database()
     user = db.get_user_address_by_name("Sergii")
+
     assert user[0][0] == "Maydan Nezalezhnosti 1"
     assert user[0][1] == "Kyiv"
     assert user[0][2] == "3127"
@@ -32,9 +33,43 @@ def test_product_qnt_update():
     db = Database()
     db.update_product_qnt_by_id(1, 25) # updating quantity
     water_qnt = db.select_product_qnt_by_id(1) # get the quantity of the item
+
     assert water_qnt[0][0] == 25 # verifies that quantity equal to the value that we provided
 
 
-    
+ # test #5
+@pytest.mark.database
+def test_product_insert():
+    db = Database()
+    db.insert_product(4, 'печиво', 'солодке', 30)
+    cookie_qnt = db.select_product_qnt_by_id(4)
 
-    
+    assert cookie_qnt[0][0] == 30 # verifies that quantity equal to the value that we provided
+
+
+
+ # tests #5
+@pytest.mark.database
+def test_product_delete():
+    db = Database()
+    db.insert_product(99, 'test', 'data', 999) # create temp test data in the table
+    db.delete_product_by_id(99) # delete temp test data from the table
+    qnt = db.select_product_qnt_by_id(99) # verify that created test data is deleted
+
+    assert len(qnt) == 0 # confirm that test data doesn't exist in the table
+
+
+# test #6
+@pytest.mark.database
+def test_detailed_orders():
+    db = Database()
+    orders = db.get_detailed_orders()
+    print("Order details:", orders)
+    # check quantity of orders equal to 1
+    assert len(orders) == 1
+
+    # check structure od data
+    assert orders[0][0] == 1
+    assert orders[0][1] == "Sergii"
+    assert orders[0][2] == "солодка вода"
+    assert orders[0][3] == "з цукром"
